fe8f668b70e0448813583f892caa7990a5ca5758
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-12-13 10:32:14 +0200

    Tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -13,6 +13,10 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+Cypress.on('uncaught:exception', () => {
+  return false;
+})
+
 // Import commands.js using ES2015 syntax:
 import './commands'
 
