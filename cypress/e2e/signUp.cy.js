8ce462603f62ec9c663a7c1ff6d63043b0ff7d9d
Karolina Vlasova <carolina150897@gmail.com>
2022-12-14 20:47:56 +0100

    solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,22 +1,20 @@
-import { faker } from '@faker-js/faker';
-const random = Math.random().toString().slice(2, 6);
-const username = faker.internet.userName() + '_' + random;
-const email = `${username}@mail.com`;
-const password = '12345Qwert!';
+const { generateUser } = require('../support/generate');
 
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const user = generateUser();
+    
     cy.visit('https://react-redux.realworld.io/');
 
     cy.get(':nth-child(3) > .nav-link').should('contain', 'Sign up').click();
     cy.url().should('include', '/register');
     cy.get('h1.text-xs-center').should('contain', 'Sign Up');
 
-    cy.get(':nth-child(1) > .form-control').type(username);
-    cy.get(':nth-child(2) > .form-control').type(email);
-    cy.get(':nth-child(3) > .form-control').type(password);
-    cy.get('.btn').should('contain', 'Sign up').click();
+    cy.get(':nth-child(1) > .form-control').type(user.username);
+    cy.get(':nth-child(2) > .form-control').type(user.email);
+    cy.get(':nth-child(3) > .form-control').type(user.password);
+    cy.get('.btn').should('contain', 'Sign in').click();
 
-    cy.get(':nth-child(4) > .nav-link').should('contain', username);
+    cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
   });
 });
