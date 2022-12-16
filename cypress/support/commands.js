0f98ae708ca9dc0b7fc01de1d0ac638000c15289
Oleksandra Vlasyuk <oleksandra.vl30@gmail.com>
2022-12-16 19:14:53 +0200

    Solution

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,15 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const faker = require('faker');
+
+function generateUser() {
+  const random = Math.random().toString().slice(2, 6);
+  const username = faker.internet.userName();
+  const email = faker.internet.email();
+  const password = '12345Qwert!';
+
+  return { email, password, username };
+}
+
+module.exports = { generateUser };
