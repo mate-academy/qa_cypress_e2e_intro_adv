2626cef56ec054a7cd6f2d5d46225db965b1f511
Roman Paliy <paliygamingua@gmail.com>
2022-12-18 16:13:40 +0200

    mySolution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,21 @@
+const { generateUser } = require('../support/generate');
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const user = generateUser();
 
+    cy.visit('https://react-redux.realworld.io');
+    cy.get('[href="#register"]')
+      .click();
+    cy.get('[placeholder="Username"]')
+      .type(`${user.userName}`);
+    cy.get('[placeholder="Email"]')
+      .type(`${user.email}`);
+    cy.get('[placeholder="Password"]')
+      .type(`${user.password}`);
+    cy.get('[type="submit"]')
+      .click();
+    cy.get(`[href="#@${user.userName}"]`)
+      .should('contain', `${user.userName}`);
   });
 });
