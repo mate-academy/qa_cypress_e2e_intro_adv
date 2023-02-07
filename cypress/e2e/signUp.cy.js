c1c53149c7678e4684f8c0b2db6648979980f892
Solomia Biletska <solomiya100506@gmail.com>
2023-02-08 00:43:31 +0200

    Solution e2e_intro adv

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -2,7 +2,7 @@ const { generateUser } = require('../support/generate');
 
 describe('Sign Up page', () => {
   beforeEach(() => {
-    cy.visit('https://react-redux.realworld.io')
+    cy.visit('https://react-redux.realworld.io');
   });
 
   it('should provide an ability to register new account', () => {
