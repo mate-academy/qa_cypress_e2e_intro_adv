ea0a56c00f5cbb06ee99a4bcf5b9ab3d9ac3ed53
Lesia Okseniuk <hrushkachereshnia@gmail.com>
2023-02-11 21:16:21 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
+const faker = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: `https://react-redux.realworld.io`,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
