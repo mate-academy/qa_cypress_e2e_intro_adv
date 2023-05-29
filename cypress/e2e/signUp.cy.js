fbdc40d3ab4497bba652203bd7af7d5f919d9407
Denys Shyshlakov <444.spam@gmail.com>
2023-05-29 22:23:37 +0300

    Cypress intro adv

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,29 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types="cypress" />
+const { generateUser } = require('../support/generate');
 
+describe('Sign Up page', () => {
+  before(() => {cy.visit('https://react-redux.realworld.io');});
+  it('should provide an ability to register new account', () => {
+    const user = generateUser();
+    
+  
+    cy.get(':nth-child(3) > .nav-link')
+    .click ();
+    cy.contains('h1', 'Sign Up')
+    .should('be.visible');
+    cy.url()
+    .should('include', 'register');
+    cy.get(':nth-child(1) > .form-control')  
+    .type (user.username);
+    cy.get(':nth-child(2) > .form-control')
+    .type (user.email);
+    cy.get(':nth-child(3) > .form-control')
+    .type(user.password);
+    cy.get('.btn')
+    .click();
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain',user.username)
   });
 });
+
+
