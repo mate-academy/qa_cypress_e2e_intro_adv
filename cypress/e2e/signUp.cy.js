4ca35acf1dd985abc890ea9cb7d469203cadeba9
Vitalia <89596289+VitaDemian@users.noreply.github.com>
2023-05-29 17:40:04 +0300

    e2e intro adv

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,30 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+const { generateUser } = require('../support/generate');
 
+describe('Sign Up page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/register?_k=4fftua')
+  });
+
+  it('should provide an ability to register new account', () => {
+    const {username,  email, password} = generateUser();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type(username);
+
+    cy.get(':nth-child(2) > .form-control')
+      .type(email);
+
+    cy.get(':nth-child(3) > .form-control')
+      .type(password);
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', username);
+    
   });
 });
+
+
+
