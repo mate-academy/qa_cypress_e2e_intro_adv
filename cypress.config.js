a9590c9ce5702e4344975b9abee6d8d3aefe5f85
Yuliia Vereshko <pyliatko@gmail.com>
2023-05-29 15:38:36 +0200

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    "baseUrl": "https://react-redux.realworld.io",
+    "viewportWidth": 1024,
+    "viewportHeight": 768,
+  }
 });
--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,19 @@
+/// <reference types="cypress" />
+
+const { generateUser } = require('../support/generate');
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
-
+    const user = generateUser();
+    cy.visit('/#/register')
+    cy.get('[placeholder="Username"]')
+      .type(user.username)
+    cy.get('[placeholder="Email"]')
+      .type(user.email)
+    cy.get('[placeholder="Password"]')
+      .type(user.password);
+    cy.contains('.btn', 'Sign in')
+      .click();
+    cy.get('.navbar')
+      .should('contain', user.username);
   });
 });
