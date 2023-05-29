1d15475e9b56309ff97ca927ee35a199aaa8c2fc
Yekateryna <keshareo@gmail.com>
2023-05-29 13:05:53 +0300

    sign up page tests with faker

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,27 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+const { generateUser } = require('../support/generateUser');
 
+describe('Sign Up page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/register')
+  })
+
+  it('should provide an ability to register new account', () => {
+    const { email, password, username } = generateUser();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type(username)
+
+    cy.get(':nth-child(2) > .form-control')
+      .type(email)
+
+    cy.get(':nth-child(3) > .form-control')
+      .type(password)
+
+    cy.contains('.btn', 'Sign in') // should be here 'sign up' text instead of 'sign in' ?
+      .click()
+
+    cy.contains(':nth-child(4) > .nav-link', username)
   });
 });
+
+
