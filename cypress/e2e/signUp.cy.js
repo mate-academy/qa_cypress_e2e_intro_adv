4052b2e951f7759ef0476f25d681fd06d2e5c385
Anton Kalnohuz <akalnoguz@gmail.com>
2023-05-30 12:58:31 +0300

    Changed selector

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -11,11 +11,11 @@ describe('Sign Up page', () => {
     cy.contains('a', 'Sign up')
       .click();
 
-    cy.get(':nth-child(1) > .form-control')
+    cy.get('input[placeholder="Username"]')
       .type(username)
-    cy.get(':nth-child(2) > .form-control')
+    cy.get('input[placeholder="Email"]')
       .type(email)
-    cy.get(':nth-child(3) > .form-control')
+    cy.get('input[placeholder="Password"]')
       .type(password)
     cy.contains('.btn', 'Sign in')
       .click();
