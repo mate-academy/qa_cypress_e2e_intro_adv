2f85fcaf7a97fc28b1e942a11f64f8ecf3170dae
OlehHoryk <skarllet8@gmail.com>
2023-05-30 14:35:28 +0300

    oleh222

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -4,11 +4,11 @@ describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
     const user = generateUser();
     cy.visit ('https://react-redux.realworld.io/#/register')
-    cy.get(':nth-child(1) > .form-control')
+    cy.get('[placeholder="Username"')
     .type(user.username)
-    cy.get(':nth-child(2) > .form-control')
+    cy.get('[placeholder="Email"')
     .type(user.email)
-    cy.get(':nth-child(3) > .form-control')
+    cy.get('[placeholder="Password"')
     .type(user.password)
     cy.get('.btn')
     .click()
