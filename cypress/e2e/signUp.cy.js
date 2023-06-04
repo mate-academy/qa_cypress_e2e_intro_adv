afa060664369b6cfe82322be1fc3a9c99944fb33
ann-yurkevych <linaostaphuk@gmail.com>
2023-06-04 16:10:37 +0300

    Auto tests with cypress registration page

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,23 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types="cypress" />
 
+const { generateUser } = require('../support/generate');
+
+describe('Sign Up page', () => {
+  beforeEach(() => {
+    cy.visit('/#/register');
+  });
+
+  it('should provide an ability to Sign up', () => {
+    const {email, username, password} = generateUser();
+      cy.get(':nth-child(1) > .form-control')
+      .type(username);
+      cy.get(':nth-child(2) > .form-control')
+      .type(email);
+      cy.get(':nth-child(3) > .form-control')
+      .type(password);
+      cy.get(':nth-child(4) > .nav-link')
+      .should('contain', username);
+      cy.contains('.nav-link', 'Sign up')
+      .click();
   });
 });
---
+++ cypress/support/generate.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+const faker = require('faker');
+
+function generateUser() {
+    const random = Math.random().toString().slice(2, 6);
+    const faker = require('faker');
+    const username = faker.internet.userName() + '_' + random;
+    const email = `${username}@mail.com`;
+    const password = '12345Qwert!';
+    return { email, password, username };
+}
+
+module.exports = { generateUser };
