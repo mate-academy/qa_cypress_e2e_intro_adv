5bbe31ff41aa7bb56f63af74fe1b8582e2b3f5f1
mike <mykolaiipobihailo@gmail.com>
2023-09-22 14:35:09 +0300

    Cypress - Inroduction- Advanced - Solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,26 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types='cypress' />
 
+const { generateUser } = require('../support/generate');
+
+describe('Sign Up page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should provide an ability to register new account', () => {
+    const { email, password, username } = generateUser();
+
+    cy.contains('a', 'Sign up')
+      .click();
+    cy.findByPlaceholder('Username')
+      .type(username);
+    cy.findByPlaceholder('Email')
+      .type(email);
+    cy.findByPlaceholder('Password')
+      .type(password);
+    cy.contains('button', 'Sign in')
+      .click();
+    cy.get('.nav-link')
+      .should('contain', username);
   });
 });
