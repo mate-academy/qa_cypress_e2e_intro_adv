a791fb0060ad3f52211dc18c7f7704919d309112
Nataliya Hnativ <natkahnativ@gmail.com>
2023-10-31 18:11:49 +0200

    SignUp test advanced

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,23 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types="cypress" />
 
+const { generateUser } = require("../support/generate.js");
+
+describe('Sign Up page', () => {
+  const { username, email, password } = generateUser();
+
+  it('should provide an ability to register new account', () => {
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(3) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type(username);
+    cy.get(':nth-child(2) > .form-control')
+      .type(email);
+    cy.get(':nth-child(3) > .form-control')
+      .type(password);
+    cy.get('.btn')
+      .click();
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', username);
   });
 });
