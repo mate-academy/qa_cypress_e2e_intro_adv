7a8aee14156fa7eba6d8abe6aacc3484c38bb5fb
Yevhen Boiko <angry90.jb@gmail.com>
2023-11-01 12:08:17 +0200

    Solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -6,16 +6,22 @@ describe('Sign Up page', () => {
 
     cy.visit('https://react-redux.realworld.io');
 
-    cy.get(':nth-child(3) > .nav-link').click();
+    cy.get(':nth-child(3) > .nav-link')
+      .click();
 
-    cy.get(':nth-child(1) > .form-control').type(user.username);
+    cy.get(':nth-child(1) > .form-control')
+      .type(user.username);
 
-    cy.get(':nth-child(2) > .form-control').type(user.email);
+    cy.get(':nth-child(2) > .form-control')
+      .type(user.email);
 
-    cy.get(':nth-child(3) > .form-control').type(user.password);
+    cy.get(':nth-child(3) > .form-control')
+      .type(user.password);
 
-    cy.get('.btn').click();
+    cy.get('.btn')
+      .click();
 
-    cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', user.username);
   });
 });
