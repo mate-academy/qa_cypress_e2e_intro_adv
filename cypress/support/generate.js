dc0d0aad6e27389c022eb9c4ff202760d681ce89
Alex <horonhozov@gmail.com>
2023-11-05 12:31:29 +0200

    Solution

---
+++ cypress/support/generate.js
@@ -0,0 +1,12 @@
+const faker = require('faker');
+
+function generateUser() {
+  const random = Math.random().toString().slice(2, 6);
+  const username = faker.internet.userName() + '_' + random;
+  const email = `${username}@mail.com`;
+  const password = 'password1234$#';
+
+  return { email, password, username };
+}
+
+module.exports = { generateUser };
