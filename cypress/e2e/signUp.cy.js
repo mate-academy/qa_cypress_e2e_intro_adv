84549c03fcee175db3449774f75139e986fe1ff2
Dariusz Bydolek <darek021982@gmail.com>
2023-12-04 18:20:07 +0100

    Solution v2

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -2,22 +2,16 @@ const { generateUser } = require('../support/generate');
 
 describe('Sign Up page', () => {
   before(() => {
-    cy.visit('https://react-redux.realworld.io');
+    cy.visit('/register');
   });
   it('should provide an ability to register new account', () => {
     const user = generateUser();
 
-    cy.get(':nth-child(3) > .nav-link')
-      .click();
-    cy.get(':nth-child(1) > .form-control')
-      .type(user.username);
-    cy.get(':nth-child(2) > .form-control')
-      .type(user.email);
-    cy.get(':nth-child(3) > .form-control')
-      .type(user.password);
+    cy.get('input[type="text"]').type(user.username);
+    cy.get('input[type="email"]').type(user.email);
+    cy.get('input[type="password"]').type(user.password);
     cy.contains('.btn', 'Sign in')
       .click();
-    cy.get(':nth-child(4) > .nav-link')
-      .should('contain', user.username);
+    cy.get('.navbar').should('contain', user.username);
   });
 });
