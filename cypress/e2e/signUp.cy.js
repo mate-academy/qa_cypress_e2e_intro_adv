bb163cba70b8a35e685d9eebacfdf620ee850422
Olesia Semen <semenolesa01@gmail.com>
2023-12-10 12:58:08 +0200

    Test

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,17 +1,17 @@
 const { generateUser } = require('../support/generate');
 
 describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+  it('should provide an ability to register a new account', () => {
     cy.visit('https://react-redux.realworld.io');
     const user = generateUser();
+
     cy.get(':nth-child(3) > .nav-link').click();
-    cy.get(':nth-child(1) > .form-control').click()
-      .type(user.username);
-    cy.get(':nth-child(2) > .form-control').click()
-      .type(user.email);
-    cy.get(':nth-child(3) > .form-control').click()
-      .type(user.password);
+
+    cy.get(':nth-child(1) > .form-control').type(user.username);
+    cy.get(':nth-child(2) > .form-control').type(user.email);
+    cy.get(':nth-child(3) > .form-control').type(user.password);
     cy.get('.btn').click();
-    cy.get(':nth-child(4) > .nav-link').should('exist');
+
+    cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
   });
 });
