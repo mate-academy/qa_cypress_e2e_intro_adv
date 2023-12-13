afc8e304a4c567833be2228836bbbdc26269a2f8
Dawid_Antkiewicz <dawidantkiew@gmail.com>
2023-12-13 15:04:20 +0100

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://react-redux.realworld.io',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        generateUser() {
+          const random = Math.random().toString().slice(2, 6);
+          const username = faker.internet.userName() + '_' + random;
+          const email = `${username}@mail.com`;
+          const password = '12345Qwert!';
+        
+          return { email, password, username };
+        }
+      })
     }
   }
 });
