aa54458e2cfcf68d3f3441b2b91000652570e98b
Oksana Kachmar <profiscribe@gmail.com>
2024-03-12 19:13:07 +0200

    add changes

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -3,13 +3,17 @@
 const { generateUser } = require('../support/generate');
 
 describe('Sign Up page', () => {
-  it('should', () => {
+  beforeEach(() => {
+    cy.visit(`https://react-redux.realworld.io/#/register`);
+  });
+  it('should provide an ability to register new account', () => {
     const { email, password, username } = generateUser();
 
     cy.visit('https://react-redux.realworld.io/#/register?_k=a5xtmi');
     cy.get('input[placeholder="Username"]').type(username);
     cy.get('input[placeholder="Email"]').type(email);
     cy.get('input[placeholder="Password"]').type(password);
-    cy.get('button[type="submit"]').should('contain.text', 'Sign in').click();
+    cy.get('button[type="submit"]').click();
+    cy.get('a[class="nav-link"]').should('contain.text', username);
   });
 });
