fa32c2ba6d2bdbd326a94aac6583d5484d0b694d
Olena Lissova <lena.lissova@gmail.com>
2024-03-12 16:37:19 +0100

    Solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,29 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types="cypress" />
 
+const { generateUser } = require('../support/generate');
+
+describe('Sign Up page', () => {
+  beforeEach(() => {
+    cy.visit('/#/register');
+  });
+
+  it('should provide an ability to register new account', () => {
+    const { email, password, username } = generateUser();
+    cy.get('h1')
+      .should('contain.text', 'Sign Up');
+
+    cy.get('[placeholder=Username]')
+      .type(username);
+
+    cy.get('[placeholder=Email]')
+      .type(email);
+
+    cy.get('[placeholder=Password]')
+      .type(password);
+
+    cy.get('.btn').click();
+
+    cy.get('.nav-link')
+      .should('contain.text', username);
   });
 });
