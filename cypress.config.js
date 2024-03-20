a1b4378dbcdb2fa0c49b6030e7c6195fb5ab0b4c
Katerina Poliit <alexispoliyit@gmail.com>
2024-03-20 19:23:05 +0300

    'Solution '

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://react-redux.realworld.io',
+    viewportWidth: 1024,
+    viewportHeight: 768,
+    defaultCommandTimeout: 4000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
