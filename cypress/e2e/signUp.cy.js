d5b5da43ca207bfee4a7e29b175752205be37454
ecrn erkl <ecrin.erkliii@gmail.com>
2024-03-20 10:29:42 +0200

    HW2

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,37 @@
+const { generateUser } = require("../support/generate");
+
 describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    });
+  
+    it('should provide an ability to register new account', () => {
+      const {username, email, password} = generateUser()
+    
+    cy.contains('a', 'Sign up')
+     .click()
+
+    cy.url()
+     .should('include', '/register')
+
+    cy.get('h1')
+     .should('contain.text', 'Sign Up')
+
+    cy.get('[placeholder=Username]')
+     .type(username)
+
+     cy.get('[placeholder=Email]')
+     .type(email)
+
+    cy.get('[placeholder=Password]')
+    .type(password)
+
+    cy.get('button[type="submit"]')
+     .click()
+
+    cy.get('[class="navbar navbar-light"]')
+     .last()
+     .should('contain.text', `${username}`) 
 
   });
 });
