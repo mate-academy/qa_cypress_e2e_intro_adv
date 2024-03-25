5efd06893af997735e919bb1ff33e3f5d2580616
Administrator <i.chernyavskyisoftum.io@gmail.com>
2024-03-25 18:17:49 +0200

    Sol1

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
+
+
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://react-redux.realworld.io/#',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
