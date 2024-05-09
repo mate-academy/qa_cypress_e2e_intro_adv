b699358677fd40c60824da29ebf935d80ae4e824
Oleksii <aleksey.gudilkin@gmail.com>
2024-05-09 15:42:10 +0300

    sign up tests added

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,16 @@
+/// <reference types="cypress" />
+import { generateUser } from '../support/generate';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const { email, username, password } = generateUser();
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(3) > .nav-link').click();
+    cy.get(':nth-child(1) > .form-control').type(username);
+    cy.get(':nth-child(2) > .form-control').type(email);
+    cy.get(':nth-child(3) > .form-control').type(password);
+    cy.get('.btn').click();
 
+    cy.get(':nth-child(4) > .nav-link').should('contain', username);
   });
 });
