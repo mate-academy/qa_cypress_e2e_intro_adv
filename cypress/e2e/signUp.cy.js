0e06be0cddc1509f8cde8b68742fb18467d01e16
Tanya <tmfancy@ukr.net>
2024-05-09 18:13:43 +0300

    SignUp

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,13 @@
+import { generateUser } from '../support/generate';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
-
+    const { email, password, username } = generateUser();
+    cy.visit('https://react-redux.realworld.io/#/register');
+    cy.get(':nth-child(1) > .form-control').type(username);
+    cy.get(':nth-child(2) > .form-control').type(email);
+    cy.get(':nth-child(3) > .form-control').type(password);
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('contain', username);
   });
 });
