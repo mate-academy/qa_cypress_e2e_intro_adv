e7dff2d465d41a85b9d4e99f5e21e473e82d151c
Oleg Voloshyn <voloshyn.oleg.oleg@gmail.com>
2024-07-25 15:56:23 +0300

    My work

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,14 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types="cypress" />
+import { generateUser } from "../support/generate";
 
+describe("Sign Up page", () => {
+  it("should provide an ability to register new account", () => {
+    const user = generateUser();
+    cy.visit("https://react-redux.realworld.io/#/register?_k=coa3vs");
+    cy.get(":nth-child(1) > .form-control").type(user.username);
+    cy.get(":nth-child(2) > .form-control").type(user.email);
+    cy.get(":nth-child(3) > .form-control").type(user.password);
+    cy.get(".btn").click();
+    cy.get(":nth-child(4) > .nav-link").should("contain", user.username);
   });
 });
