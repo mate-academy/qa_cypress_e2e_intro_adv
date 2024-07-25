115adab9fd0a72e9da7fbf6522eb5f1d9b0a4895
yuliayahusta <yuliayahusta@gmail.com>
2024-07-25 20:05:50 +0300

    Automate sign up test

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,16 @@
+import { generateUser } from '../support/generate';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const user = generateUser();
 
+    cy.visit('https://react-redux.realworld.io/#/register');
+    cy.get(':nth-child(1) > .form-control').type(user.username);
+    cy.get(':nth-child(2) > .form-control').type(user.email);
+    cy.get(':nth-child(3) > .form-control').type(user.password);
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain.text', user.username);
   });
 });
