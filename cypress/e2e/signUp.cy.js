2dc42ac8092710e28a30afbef85a2d9c7a0d26b2
MariiaKalezhniuk <mzaharova318@gmail.com>
2024-07-25 15:07:41 +0300

    Solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,15 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+import { generateUser } from '../support/generate';
 
+describe('Sign Up page', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io/#/register?_k=1t7pel');
+  });
+  it('should provide an ability to register new account', () => {
+    const user = generateUser();
+    cy.get(':nth-child(1) > .form-control').type(user.username);
+    cy.get(':nth-child(2) > .form-control').type(user.email);
+    cy.get(':nth-child(3) > .form-control').type(user.password);
+    cy.get('.btn').click();
+    cy.get('.navbar').should('contain', user.username);
   });
 });
