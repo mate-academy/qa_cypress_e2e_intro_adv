f55f3e395aa67c2570ab9329457574069b1ce93b
Anna Hutnichenko <gutnichenkoo@gmail.com>
2024-07-26 16:45:18 +0300

    added new tests

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,20 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+import { generateUser } from '../support/generate';
 
+describe('Sign Up page', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io');
+  });
+
+  it('should provide an ability to register new account', () => {
+    const user = generateUser();
+
+    cy.get('[href="#register"]').click();
+
+    cy.get('[placeholder="Username"]').type(user.username);
+    cy.get('[placeholder="Email"]').type(user.email);
+    cy.get('[placeholder="Password"]').type(user.password);
+    cy.get('[type="submit"]').click();
+
+    cy.get('.nav-link').should('contain', user.username);
   });
 });
