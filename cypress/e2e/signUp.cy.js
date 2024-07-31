07d9b4f79ceabd9be4b110c7aa745a40e4ba7f23
MishaKluch <nimamiha@gmail.com>
2024-07-31 15:25:25 +0300

    Test to Sign Up

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,17 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+import { generateUser } from '../support/generate';
 
+describe('Sign Up page', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io/#/register?_k=v0avdp');
+  });
+
+  it('should provide an ability to register new account', () => {
+    const user = generateUser();
+    cy.findByPlaceholder('Username').type(user.username);
+    cy.findByPlaceholder('Email').type(user.email);
+    cy.findByPlaceholder('Password').type(user.password);
+    cy.get('.btn').contains('Sign in').click();
+    
+    cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
   });
 });
