eadb6ef5626ce519ccde66ee35167ec5d908d288
Ilya Pulcho <pulcho91@gmail.com>
2024-08-04 15:54:14 +0300

    Solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,24 +1,22 @@
 import { generateUser } from '../support/generate';
 
-describe('User Registration and Login Tests', () => {
+describe('User Registration', () => {
   beforeEach(() => {
     cy.visit('https://react-redux.realworld.io');
   });
 
-  describe('Sign Up page', () => {
-    it('should provide an ability to register new account', () => {
-      cy.get(':nth-child(3) > .nav-link').click();
-      const user = generateUser();
+  it('should provide an ability to register new account', () => {
+    cy.get(':nth-child(3) > .nav-link').click();
 
-      cy.get(':nth-child(1) > .form-control').type(user.username);
-      cy.get(':nth-child(2) > .form-control').type(user.email);
-      cy.get(':nth-child(3) > .form-control').type(user.password);
+    const user = generateUser();
 
-      cy.get('.btn')
-        .click();
-      cy.get(':nth-child(4) > .nav-link')
-        .should('be.visible')
-        .and('contain.text', user.username);
-    });
+    cy.get(':nth-child(1) > .form-control').type(user.username);
+    cy.get(':nth-child(2) > .form-control').type(user.email);
+    cy.get(':nth-child(3) > .form-control').type(user.password);
+
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link')
+      .should('be.visible')
+      .and('contain.text', user.username);
   });
 });
