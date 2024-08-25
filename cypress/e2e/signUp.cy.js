8ce99b0c5825beda7893485a98b0b965de50938d
Viacheslav Kovryzhykh <v.kovryzhnykh@gmail.com>
2024-08-25 15:41:25 +0200

    Update test

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,14 @@
+import { generateUser } from '../support/generate';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
-
+    const user = generateUser();
+    // eslint-disable-next-line max-len
+    cy.visit('https://react-redux.realworld.io/#/register', { timeout: 120000 });
+    cy.get('input[placeholder="Username"]').type(user.username);
+    cy.get('input[placeholder="Email"]').type(user.email);
+    cy.get('input[placeholder="Password"]').type(user.password);
+    cy.get('button[type="submit"]').click();
+    cy.get('.navbar').should('contain', user.username);
   });
 });
