91725302287430c6d1301af1713aed51dec59a4e
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-09-03 15:27:17 +0300

    Advanced

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,21 @@
+/// <reference types="cypress" />
+
+import { generateUser } from '../support/generate';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const user = generateUser();
 
+    // Перехід на сторінку реєстрації з правильним URL
+    cy.visit('/#/register');
+
+    // Взаємодія з елементами форми реєстрації
+    cy.get('input[placeholder="Username"]').type(user.username);
+    cy.get('input[placeholder="Email"]').type(user.email);
+    cy.get('input[placeholder="Password"]').type(user.password);
+    cy.get('button[type="submit"]').click();
+
+    // Перевірка успішної реєстрації
+    cy.get('.navbar').should('contain', user.username);
   });
 });
---
+++ cypress/support/generate.js
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker';
+
+export function generateUser() {
+  const randomNumber = Math.random().toString().slice(2, 6);
+  const username = faker.internet.userName() + '_' + randomNumber;
+  const email = `${username}@mail.com`;
+  const password = '12345Qwert!';
+
+  return { email, password, username };
+}
