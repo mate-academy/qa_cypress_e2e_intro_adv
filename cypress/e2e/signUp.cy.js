c7c328257688fdb42d7ac5e53dcbebc0284296d0
Meyer Dobrokhotov <meyerdobrokhotov@gmail.com>
2024-09-13 16:03:02 +0200

    Solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,15 @@
+import { generateUser } from '../support/generate.js';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const user = generateUser();
 
+    cy.visit('/#/register?_k=t01ofk');
+    cy.get('[placeholder="Username"]').type(user.username);
+    cy.get('[placeholder="Email"]').type(user.email);
+    cy.get('[placeholder="Password"]').type(user.password);
+    cy.get('[type="submit"]').click();
+    cy.get('[class="navbar-brand"]').click();
+    cy.get('.pull-xs-right.navbar-nav').should('contain', user.username);
   });
 });
