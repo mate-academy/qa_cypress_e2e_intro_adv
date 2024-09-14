1b2c097ec3cc6bf272e2d5c939aec13b372e68e1
maksim-atanasov <atanasovmaksim56@gmail.com>
2024-09-14 13:01:33 +0300

    solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,17 @@
+const { generateUser } = require('../support/generate');
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    cy.visit('/');
 
+    cy.contains('a', 'Sign up').click();
+
+    const user = generateUser();
+
+    cy.get('input[placeholder="Username"]').type(user.username);
+    cy.get('input[placeholder="Email"]').type(user.email);
+    cy.get('input[placeholder="Password"]').type(user.password);
+
+    cy.contains('button', 'Sign in').click();
   });
 });
