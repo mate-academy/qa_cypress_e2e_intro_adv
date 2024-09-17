ee5a82d2c8db76fe9dbadf7bc71b6a7bca527048
Bartłomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-09-17 12:37:00 +0200

    Task resolved

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,18 @@
+import { generateUser } from '../support/generate';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
-
+    const { username, email, password } = generateUser();
+    cy.visit('https://react-redux.realworld.io/#/register');
+    cy.get('[placeholder="Username"]')
+      .type(username);
+    cy.get('[placeholder="Email"]')
+      .type(email);
+    cy.get('[placeholder="Password"]')
+      .type(password);
+    cy.contains('button', 'Sign in')
+      .click();
+    cy.get('a.nav-link')
+      .should('contain', username);
   });
 });
