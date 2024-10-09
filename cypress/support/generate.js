d99b5ef68812ca55b7163d1c743c3575afbb6e92
Oksana Tereshchuk <oxanatereschuk@gmail.com>
2024-10-09 17:41:03 +0300

    adv3

--- cypress/support/generate.js
+++ cypress/support/generate.js
@@ -4,7 +4,7 @@ function generateUser() {
   const randomNumber = Math.random().toString().slice(2, 6);
   const username = faker.internet.userName() + '_' + randomNumber;
   const email = `${username}@mail.com`;
-  const password = faker.internet.password();
+  const password = '12345Qwert!';
 
   return { email, password, username };
 }
