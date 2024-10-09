54f079ef046ee30da1b382b8b4531dbd1b050c80
Iryna Turaieva <irina.onlyjob@gmail.com>
2024-10-09 21:14:57 +0200

    Add sign-up test

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,13 @@
+import { generateUser } from '../support/generate';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
-
+    const user = generateUser();
+    cy.visit('https://react-redux.realworld.io/#/register?_k=rkc761');
+    cy.get('input[placeholder="Username"]').type(user.username);
+    cy.get('input[placeholder="Email"]').type(user.email);
+    cy.get('input[placeholder="Password"]').type(user.password);
+    cy.get('button[type="submit"]').click();
+    cy.get('.nav-link').should('contain', user.username);
   });
 });
