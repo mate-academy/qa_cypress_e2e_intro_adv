658405611fb2baae67b6938b18121e18a4bfabdb
Vita <st.vitalina666@gmail.com>
2024-10-09 16:30:34 +0300

    Solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,29 @@
+import { generateUser } from '../support/generate';
+
+Cypress.on('uncaught:exception', () => {
+  return false;
+});
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const { email, username, password } = generateUser();
 
+    cy.visit('https://react-redux.realworld.io/#/register?_k=spysa9');
+
+    cy.get('[placeholder=Username]')
+      .type(username);
+
+    cy.get('[placeholder=Email]')
+      .type(email);
+
+    cy.get('[placeholder=Password]')
+      .type(password);
+
+    cy.get('button[type=submit]')
+      .should('exist').click();
+
+    cy.url().should('not.contain', '/register');
+
+    cy.get('a.nav-link').contains(username).should('exist');
   });
 });
