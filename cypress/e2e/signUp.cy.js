786f73d11dd7996582688b6c3bf4a00bda6fed7a
Sofi Mamzer <sofiiamamzer06@gmail.com>
2024-10-09 15:12:41 +0300

    Sign Up

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,17 @@
+/// <reference types="cypress" />
+
+import { generateUser } from '../support/generate';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const user = generateUser();
 
+    cy.visit('user/register');
+    cy.get(':nth-child(1) > .form-control').type(user.username);
+    cy.get(':nth-child(2) > .form-control').type(user.email);
+    cy.get(':nth-child(3) > .form-control').type(user.password);
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link');
+    cy.should('contain', user.username.toLowerCase());
   });
 });
