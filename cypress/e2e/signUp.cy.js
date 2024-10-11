2b6f292d8227863ad7feab44da1f4917702278f8
Mikola Lemeshev <mate.lemeshev@gmail.com>
2024-10-11 12:51:34 +0300

    solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,26 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types="cypress" />
 
+const username = 'User ' + Date.now();
+const email = username + '@i.ua';
+const password = 'User123$';
+
+describe('Sign Up page', () => {
+  beforeEach(() => {
+    cy.visit('https://conduit.mate.academy/');
+  });
+
+  it('should click on Sign Up and create a new account', () => {
+    cy.contains('a', 'Sign up')
+      .should('be.visible')
+      .click();
+
+    cy.get(':nth-child(1) > .form-control').type(username);
+    cy.get(':nth-child(2) > .form-control').type(email);
+    cy.get(':nth-child(3) > .form-control').type(password);
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.nav-link')
+      .should('contain.text', username.toLowerCase())
+      .and('be.visible');
   });
 });
