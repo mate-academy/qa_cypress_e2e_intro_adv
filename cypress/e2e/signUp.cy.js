d2662c2e9f91fd0864fea22bb9c17a239a359011
Mikola Lemeshev <mate.lemeshev@gmail.com>
2024-10-22 18:11:05 +0300

    solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,26 +1,25 @@
 /// <reference types="cypress" />
 
-const username = 'User ' + Date.now();
-const email = username + '@i.ua';
-const password = 'User123$';
+import { generateUser } from '../support/generate';
 
 describe('Sign Up page', () => {
-  beforeEach(() => {
-    cy.visit('https://conduit.mate.academy/');
-  });
+  const user = generateUser();
 
   it('should click on Sign Up and create a new account', () => {
+    cy.visit('https://conduit.mate.academy/');
     cy.contains('a', 'Sign up')
       .should('be.visible')
       .click();
 
-    cy.get(':nth-child(1) > .form-control').type(username);
-    cy.get(':nth-child(2) > .form-control').type(email);
-    cy.get(':nth-child(3) > .form-control').type(password);
+    cy.get(':nth-child(1) > .form-control').type(user.username);
+
+    cy.get(':nth-child(2) > .form-control').type(user.email);
+
+    cy.get(':nth-child(3) > .form-control').type(user.password);
     cy.get('button[type="submit"]').click();
 
     cy.get('.nav-link')
-      .should('contain.text', username.toLowerCase())
+      .should('contain.text', user.username.toLowerCase())
       .and('be.visible');
   });
 });
