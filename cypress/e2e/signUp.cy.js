188d36a1355ce8adeb8076fc40e006a5258b2d59
Galers <galosaandrew@gmail.com>
2025-01-20 18:50:52 +0200

    tests: Add tests

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,18 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types='cypress' />
+import { generateUser } from '../support/generate';
 
+describe('Sign Up page', () => {
+  const { username, email, password } = generateUser();
+
+  it('should provide an ability to register new account', () => {
+    cy.visit('/user/register');
+
+    cy.getByPlaceholder('Username', username);
+    cy.getByPlaceholder('Email', email);
+    cy.getByPlaceholder('Password', password);
+    cy.get('button[type="submit"]').click();
+    cy.get('a[class=nav-link]')
+      .contains(username.toLowerCase())
+      .should('be.visible');
   });
 });
